fc3aa1d89ce52c0a69bd9ea06c2665df939bfd8e
Nakanishi <45790603+Nakanishi123@users.noreply.github.com>
2023-08-11 11:33:01 +0900

    first commit

---
+++ src/components/viewer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <div id="image-container">
+        <img class="image" id="imgLeft" src="null" alt="">
+        <img class="image" id="imgRight" src="null" alt="">
+    </div>
+</template>
+
+<style scoped>
+.image {
+    width: 50vh;
+    height: 100vh;
+}
+
+#image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+</style>
